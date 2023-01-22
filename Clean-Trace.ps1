98fe369e69b8a997e8ed0dc81a1fee76fb1ccd8b
Salvatore || Mian <113462615+Mian-211@users.noreply.github.com>
2023-01-22 02:37:18 -0800

    Add files via upload

---
+++ Clean-Trace.ps1
@@ -0,0 +1,15 @@
+function Clean-Trace { 
+
+# empty temp folder
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# delete run box history
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+
+# Delete powershell history
+Remove-Item (Get-PSreadlineOption).HistorySavePath
+
+# Empty recycle bin
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+}
